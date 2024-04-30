ffbfb4c14c7e375535b6e530ac278045df2b9c07
Amar Begovic <amar.begovic123@hotmail.com>
2024-04-30 23:16:17 +0200

    .env and database connections

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 require 'functions.php';
 require 'router.php';
+
+// Database connection.
+$dsn = 'mysql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_NAME') . ';charset=utf8mb4';
+$pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'));
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
